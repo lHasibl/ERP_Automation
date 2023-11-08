65a27e6efc5580bec65d9f3433a4bd83426f0a8e
lHasibl <hasibuzzaman25@gmail.com>
2023-11-08 16:14:15 +0600

    HomePageButtonValidation2

--- cypress/ERP/HomePageButtonValidation.js
+++ cypress/ERP/HomePageButtonValidation.js
@@ -1,5 +1,5 @@
 ///<reference types="Cypress" />
-
+import '../../node_modules/cypress-xpath'
 describe('HomePage Button Validation', function () {
   let data;
   before(function () {
@@ -30,18 +30,51 @@ describe('HomePage Button Validation', function () {
     cy.get('span[name="name"]').contains('Md. Hasibuzzaman')
     //cy.url().should('eq', 'https://erp.bjitgroup.com/web/login')
 
-  })*/
+  })
   it('TC-03: Verify that conversation drop down menu appears when clicking on conversion button', function () {
     cy.visit(Cypress.env('url'))
     cy.get('#login').type(data.username)
     cy.get('#password').type(data.password)
     cy.get('.btn.btn-primary').click()
     cy.get('.fa.fa-comments').click()
-    cy.get('.o_mail_navbar_dropdown_top').click().contains('avcd')
-    //cy.get('.o_mail_navbar_dropdown_top > div > .active').should('have.value','ALL')
-    //cy.get('span[name="name"]').contains('Md. Hasibuzzaman')
-    //cy.url().should('eq', 'https://erp.bjitgroup.com/web/login')
-
+    cy.get('.o_mail_navbar_dropdown_top').click().contains('All')
+    
+  })*/
+  it('TC-04: Verify that conversation drop down menu appears when clicking on conversion button', function () {
+    cy.visit(Cypress.env('url'))
+    cy.get('#login').type(data.username)
+    cy.get('#password').type(data.password)
+    cy.get('.btn.btn-primary').click()
+    cy.get('.fa.fa-comments').click()
+    cy.get('.o_mail_navbar_dropdown_top').click().contains('All')
+    
+  })
+  it('TC-05: Verify that user navigate to the discussion page when clicking on Discuss button', function () {
+    cy.visit(Cypress.env('url'))
+    cy.get('#login').type(data.username)
+    cy.get('#password').type(data.password)
+    cy.get('.btn.btn-primary').click()
+    //cy.xpath("//span[@class='title'][normalize-space()='Discuss']").click()
+    cy.get(':nth-child(1) > .nav-link > .title').click()
+    cy.get('.breadcrumb').contains('Inbox')    
+  })
+  it('TC-06: Verify that user navigate to the daily Report page when clicking on Daily Report button', function () {
+    cy.visit(Cypress.env('url'))
+    cy.get('#login').type(data.username)
+    cy.get('#password').type(data.password)
+    cy.get('.btn.btn-primary').click()
+    //cy.xpath("//span[@class='title'][normalize-space()='Discuss']").click()
+    cy.get(':nth-child(2) > .nav-link > .title').click()
+    cy.get('.breadcrumb>.active').contains('Daily Report')    
+  })
+  it('TC-07: Verify that user navigate to the Calendar page when clicking on Calendar button', function () {
+    cy.visit(Cypress.env('url'))
+    cy.get('#login').type(data.username)
+    cy.get('#password').type(data.password)
+    cy.get('.btn.btn-primary').click()
+    //cy.xpath("//span[@class='title'][normalize-space()='Discuss']").click()
+    cy.get(':nth-child(3) > .nav-link > .title').click()
+    cy.get('.breadcrumb>.active').contains('Meetings')    
   })
   /*it('TC-05: Verify that user is getting logged in successfully when user name correct & password is correct', function () {
     cy.visit(Cypress.env('url'))
